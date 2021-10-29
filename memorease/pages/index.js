4a1a65dbea55db461866ecc75a6be2c2e2437f24
rachelestherchan <rachel.chan.2019@smu.edu.sg>
2021-10-30 00:06:40 +0800

    centralised the card

--- memorease/pages/index.js
+++ memorease/pages/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { db } from "../firebase/clientApp";
 import { doc, onSnapshot, collection, updateDoc } from "firebase/firestore";
 import axios from 'axios';
+<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"></link>
 
 export default function Index() {
 	const [message, setMessage] = useState("")
@@ -56,8 +57,8 @@ export default function Index() {
   
 	return (
 		<div > 
-      <div className="p-10">  
-        <div className="max-w-lg rounded overflow-hidden shadow-lg flex flex-wrap content-center">
+      <div className="p-10 bg-gray-200 flex flex-col justify-center items-center">  
+        <div className="max-w-lg rounded overflow-hidden shadow-lg bg-blue-300">
           <div className="px-6 py-4 font-mono">
           <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
             <div className="font-bold text-3xl mb-2 text-center">Reminders</div>
@@ -67,7 +68,7 @@ export default function Index() {
         :
         <img src={imageUrl} alt="reminderImage" className="pt-6"/>
       }
-            <p class="text-gray-700 text-base text-3xl mt-4 text-center">
+            <p className="text-gray-700 text-base text-3xl mt-4 text-center">
               {message}
             </p>
           </div>
