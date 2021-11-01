dc3a712ac3d0b4cd7c8563cb1407a7b51edfd559
rafeang <rafe.ang.2019@sis.smu.edu.sg>
2021-11-01 17:53:13 +0800

    text size

--- memorease/pages/index.js
+++ memorease/pages/index.js
@@ -87,16 +87,16 @@ export default function Index() {
         <div className="max-w-screen flex rounded overflow-hidden shadow-lg bg-blue-300 max-h-screen h-full">
           <div className="px-3 font-mono align-middle items-center w-full">
           <svg className="w-5 h-5 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-          <div className=" font-bold text-3xl mb-1 inline-block p-3">
+          <div className=" font-bold text-4xl mb-1 inline-block p-3">
             Reminders
             <Clock
               format={'HH:mm:ss'}
               ticking={true}
               timezone={'Asia/Singapore'}
-              className="text-3xl text-center font-semibold font-mono pl-12 float-right"
+              className="text-4xl text-center font-semibold font-mono pl-12 float-right"
             />  
           </div><hr/>
-          <p className="text-gray-700 text-base text-4xl my-2 text-center">
+          <p className="text-gray-700 text-base text-5xl my-2 text-center">
             {message}
           </p>
           {
